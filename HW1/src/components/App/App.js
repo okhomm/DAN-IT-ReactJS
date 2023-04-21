338de060a533e8949346f4e098ab68e37486aa39
Khomyshyn Oleh <khomyshyn.oleg@gmail.com>
2023-04-21 19:00:21 +0300

    all done. Final commit

--- HW1/src/components/App/App.js
+++ HW1/src/components/App/App.js
@@ -7,7 +7,8 @@ import './App.scss';
 class App extends React.Component {
 
     state = {
-        showFirstModal: false
+        showFirstModal: false,
+        showSecondModal: false
     }
 
     showFirstModalHandler = () => {
@@ -16,6 +17,28 @@ class App extends React.Component {
 
     closeFirstModalHandler = () => {
         this.setState({showFirstModal: false})
+    }
+
+    closeFirstModalFromBgHandler = (e) => {
+        e.preventDefault();
+        if (e.target.classList.contains('close')) {
+            this.closeFirstModalHandler();
+        }
+    }
+
+    showSecondModalHandler = () => {
+        this.setState({showSecondModal: true})
+    }
+
+    closeSecondModalHandler = () => {
+        this.setState({showSecondModal: false})
+    }
+
+    closeSecondModalFromBgHandler = (e) => {
+        e.preventDefault();
+        if (e.target.classList.contains('close')) {
+            this.closeSecondModalHandler();
+        }
     }
 
     render() {
@@ -27,15 +50,38 @@ class App extends React.Component {
                     text="Open first modal"
                     onClick={() => this.showFirstModalHandler()}/>
 
+                <Button
+                    backgroundColor="#ffa500"
+                    text="Open second modal"
+                    onClick={() => this.showSecondModalHandler()}/>
+
                 {this.state.showFirstModal && <Modal
                     header="Do you want to delete this file?"
                     closeButton={true}
                     text="Once you delete this file, it won't be possible to undo this action. Are you sure you want to delete it?"
                     closeModal={this.closeFirstModalHandler}
+                    closeModalFromBg={this.closeFirstModalFromBgHandler}
                     actions={
                         <>
                             <Button backgroundColor="#b3382c" text="Ok" onClick={() => this.closeFirstModalHandler()}/>
-                            <Button backgroundColor="#b3382c" text="Cancel" onClick={() => this.closeFirstModalHandler()}/>
+                            <Button backgroundColor="#b3382c" text="Cancel"
+                                    onClick={() => this.closeFirstModalHandler()}/>
+                        </>
+                    }
+                />}
+
+                {this.state.showSecondModal && <Modal
+                    header="Do you like React.js?"
+                    closeButton={false}
+                    text="Tell me, do you like studying & React.js?"
+                    closeModal={this.closeSecondModalHandler}
+                    closeModalFromBg={this.closeSecondModalFromBgHandler}
+                    actions={
+                        <>
+                            <Button backgroundColor="#666666" text="Yes"
+                                    onClick={() => this.closeSecondModalHandler()}/>
+                            <Button backgroundColor="#333333" text="Yes, I like!"
+                                    onClick={() => this.closeSecondModalHandler()}/>
                         </>
                     }
                 />}
