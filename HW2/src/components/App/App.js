9745dd1ea252b0e69e1c9759a1c9aa2c36af058a
Khomyshyn Oleh <khomyshyn.oleg@gmail.com>
2023-04-28 13:17:58 +0300

    add function getData

--- HW2/src/components/App/App.js
+++ HW2/src/components/App/App.js
@@ -2,14 +2,30 @@ import React from "react";
 import Header from "../Header/Header";
 import ProductList from "../ProductList/ProductList";
 
+
 import styles from "./App.module.css"
 
+
 class App extends React.Component {
+
+    state = {
+        products: [],
+    }
+    constructor() {
+        super();
+        this.getData();
+    }
+    getData() {
+        fetch('/data.json')
+            .then(response => response.json())
+            .then(data => this.setState({ products: data }))
+            .catch(error => console.error(error));
+    }
     render() {
         return (
             <div className={styles.wrapper}>
                 <Header/>
-                <ProductList/>
+                <ProductList products={this.state.products}/>
             </div>
         )
     }
--- HW2/src/components/ProductList/ProductList.js
+++ HW2/src/components/ProductList/ProductList.js
@@ -3,7 +3,11 @@ import Product from "../Product/Product";
 import styles from './ProductList.module.css';
 
 class ProductList extends React.Component {
+
     render() {
+        const {products} = this.props;
+        console.log(products.products[0].name)
+
         return (
             <main className={styles.container}>
                 <h2>Laptops</h2>
