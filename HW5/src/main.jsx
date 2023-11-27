aa67a54dea0ebb6419ba820842c1daafc1e215e5
Oleh Khomyshyn <khom.oleh@gmail.com>
2023-11-27 21:27:51 +0200

    fix page scroll

--- HW5/src/main.jsx
+++ HW5/src/main.jsx
@@ -1,13 +1,25 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { useLocation } from 'react-router-dom';
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import { Provider } from 'react-redux'
 import store from './store'
 import App from './App'
 
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+};
+
 ReactDOM.createRoot(document.getElementById('root')).render(
     <Provider store={store}>
       <BrowserRouter>
+      <ScrollToTop />
         <App />
       </BrowserRouter>
     </Provider>
